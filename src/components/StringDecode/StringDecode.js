c9189cc983bbbe740458244a651fd5475a7a0120
Nazarii Tsyhaniuk <Nazartsyhanyuk@gmail.com>
2023-11-23 16:22:24 -0500

    created brief structure

--- src/components/StringDecode/StringDecode.js
+++ src/components/StringDecode/StringDecode.js
@@ -1,16 +1,40 @@
+import { useState } from 'react';
+
+import debounce from 'lodash/debounce';
+
 import { useGlobal } from '../../context';
 
 import styles from './StringDecode.module.css';
 
 export const StringDecode = () => {
-  const { state } = useGlobal();
+  const { state, setGlobal } = useGlobal();
 
-  console.log(state);
+  const [encodedString, setEncodedString] = useState('');
+
+  const { decodedString } = state;
+
+  const handleChange = debounce(({ target }) => {
+    const { value } = target;
+
+    setEncodedString(value);
+  }, 500);
+
+  const handleDecode = () => {
+    const decodedString = atob(encodedString);
+
+    setGlobal({ decodedString });
+  };
 
   return (
     <div className={styles.root}>
       <h1>Enter encoded string:</h1>
-      <input type="text" value={state.decodedString} />
+      <input type="text" onChange={handleChange} />
+      <button onClick={handleDecode}>Process it!</button>
+      <p>encoded::__{encodedString}</p>
+      <div>
+        <p>decoded::</p>
+        <a href={decodedString} target="_blank">{decodedString}</a>
+      </div>
     </div>
   );
 };
