c5632eee4cd78c5993405bcc1a59c3579b50ffa2
Nazarii Tsyhaniuk <Nazartsyhanyuk@gmail.com>
2023-11-25 23:18:54 -0500

    add checking inside the TargetContainer to decide whether to use mobile styles or not

--- src/components/Layout/TargetContainer.js
+++ src/components/Layout/TargetContainer.js
@@ -1,7 +1,17 @@
+import { useGlobal } from '../../context';
+
 import styles from './Layout.module.css';
 
-export const TargetContainer = ({ size = 'lg', children }) => (
-  <div className={`${styles.targetContainer} ${styles[size]}`}>
-    {children}
-  </div>
-);
+export const TargetContainer = ({ children }) => {
+  const { state } = useGlobal();
+
+  const { isMobile } = state;
+
+  const size = isMobile ? 'md' : 'lg';
+
+  return (
+    <div className={`${styles.targetContainer} ${styles[size]}`}>
+      {children}
+    </div>
+  )
+};
